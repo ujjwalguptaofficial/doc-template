5061aeb7d8ca7f52822cec2e7d4809f00b390921
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-05-09 23:01:20 +0530

    implemnt prev,next link on tutorial, add default list

--- layouts/tutorial.vue
+++ layouts/tutorial.vue
@@ -3,8 +3,8 @@
     <div class="col-sm-4 col-md-4 col-lg-2 b-tutorial__links">
       <a
         class="col b-tutorial__links__item ripple"
-        :class="{ 'b-tutorial__links__item--active': link === activeUrl }"
-        v-for="link in links"
+        :class="{ 'b-tutorial__links__item--active': index === activeUrlIndex }"
+        v-for="(link, index) in links"
         :key="link.text"
         :href="link.url"
       >
@@ -14,13 +14,15 @@
     <div class="b-tutorial__content col-sm-8 col-md-8 col-lg-10">
       <slot></slot>
       <div class="b-tutorial__content__btns">
-        <i class="fas fa-chevron-left"></i>
-        <i class="fas fa-chevron-right"></i>
+        <i @click="goto(-1)" class="fas fa-chevron-left"></i>
+        <i @click="goto(1)" class="fas fa-chevron-right"></i>
       </div>
     </div>
   </div>
 </template>
 <script  >
+import { copyToClipboard } from "@/utils";
+
 export default {
   // tutorialContent = ``;
   // title = "";
@@ -33,13 +35,12 @@ export default {
     currentUrl() {
       return this.$route.path;
     },
-    activeUrl() {
+    activeUrlIndex() {
       const splittedPath = this.$route.path.split("/");
       const lastPath = splittedPath[splittedPath.length - 1];
-      const result = this.links.find((val) => {
+      const result = this.links.findIndex((val) => {
         return val.url === lastPath;
       });
-      console.log("result", result, lastPath);
       return result;
     },
   },
@@ -52,9 +53,36 @@ export default {
     const links = require("../content/tutorial");
     this.links = links;
   },
-  // beforeMount(){
-  //   this.pathname =
-  // }
+  mounted() {
+    hljs.highlightAll();
+    const copyHtml = `Copy <i class="margin-left-10px far fa-copy"></i>`;
+    document.querySelectorAll("pre code").forEach((el) => {
+      const div = document.createElement("div");
+      div.className = "code-copy ripple";
+      div.innerHTML = copyHtml;
+      el.parentNode.prepend(div);
+      div.onclick = () => {
+        const text = el.innerText;
+        copyToClipboard(text);
+        div.innerHTML = `Copied <i class="margin-left-10px fas fa-check"></i>`;
+        setTimeout(() => {
+          div.innerHTML = copyHtml;
+        }, 1000);
+      };
+    });
+  },
+  methods: {
+    goto(delta) {
+      const splittedPath = this.currentUrl.split("/");
+      splittedPath[splittedPath.length - 1] = this.links[
+        this.activeUrlIndex + delta
+      ].url;
+      const path = splittedPath.join("/");
+      this.$router.push({
+        path,
+      });
+    },
+  },
 };
 </script>
 <style scoped lang="scss">
@@ -62,7 +90,7 @@ export default {
   padding: 10px 10px 0 10px;
 }
 .b-tutorial__links {
-  padding-right: 20px;
+  padding-right: 50px;
 }
 .b-tutorial__links__item {
   cursor: pointer;
@@ -84,6 +112,10 @@ export default {
   display: flex;
   justify-content: space-between;
   font-size: 2rem;
-  margin-top: 20px;
+  margin-top: 30px;
+  margin-bottom: 20px;
+  i {
+    cursor: pointer;
+  }
 }
 </style>
